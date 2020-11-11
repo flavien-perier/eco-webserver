e4bd9e1cf1f2449a5d8ff04d3f2e5e705f55bed5
Flavien PERIER <flavien.perier@pm.me>
2020-11-11 16:12:17 +0100

    Reformats the HTML code

--- src/minifyHtml.js
+++ src/minifyHtml.js
@@ -3,6 +3,18 @@ const minifyJs = require("./minifyJs");
 
 module.exports = function minifyHtml(inputHtml) {
     return inputHtml
-        .replace(/^[\t ]*/gm, "")
-        .replace(/[\n\r]/gm, "");
+        // Reformatting html opening tags.
+        .replace(/<([\w\d-_]+)((?:\s+[\w\d-_]+\s*=\s*(?:["'].*?["']|[^ >]*))*)\s*?(\/)?\s*?>/sg, (_, baliseName, parameters, end) => {
+            // Reformatting tags parameters.
+            const parametersList = [];
+            parameters.replace(/([\w\d-_]+) *= *(["'].*?["']|\S*)/sg, (_, key, value) => {
+                if (value.search(" ") == -1) parametersList.push(`${key}=${value.replace(/["`]/sg, "")}`);
+                else parametersList.push(`${key}=${value}`);
+            });
+            return `<${baliseName}${parametersList.length > 0 ? " " : ""}${parametersList.join(" ")}${end || ""}>`;
+        })
+        // Reformatting html closing tags.
+        .replace(/<\s*\/\s*([\w\d-_]+)>/sg, (_, baliseName) => `</${baliseName}>`)
+        // Removes blanks between tags.
+        .replace(/>\s+</sg, "><");
 }
--- tests/minifyHtml.js
+++ tests/minifyHtml.js
@@ -2,11 +2,11 @@ const minifyHtml = require("../src/minifyHtml");
 
 describe("minifyHtml", () => {
     it("Delete indentation", () => {
-        const inputHtml = `<html>
-            <p>test</p>
-        <html>`;
+        const inputHtml = `<html lang="fr">
+            <p id="id1" class="class1 class2">test< / p><br / >
+        < / html>`;
 
-        const expectedHtml = "<html><p>test</p><html>";
+        const expectedHtml = "<html lang=fr><p id=id1 class=\"class1 class2\">test</p><br/></html>";
 
         expect(minifyHtml(inputHtml)).toEqual(expectedHtml);
     });
