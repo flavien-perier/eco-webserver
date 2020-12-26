4be47e45b7082d1228b4efd8c32a65091d4e9946
Flavien PERIER <flavien.perier@pm.me>
2020-12-26 19:56:35 +0100

    Add processing cache test

---
+++ tests/processingCache.spec.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const proxyquire = require("proxyquire").noPreserveCache();
+
+const TEST_CACHE = "./tests/test-cache";
+
+const mockConfiguration = {
+    "./configuration": { default: { cacheDir: TEST_CACHE }, "@global": true }
+};
+
+const processingCache = proxyquire("../src/processingCache", mockConfiguration);
+
+describe("processingCache", () => {
+    afterEach(() => {
+        fs.rmdirSync(TEST_CACHE, { recursive: true });
+    });
+
+    it("Cache a process and recover the value", async () => {
+        const inputProcess = "File content";
+
+        const outputProcess = "Result after work";
+
+        // Test if the data is in the cache
+        expect(processingCache.readProcessingCache(inputProcess, "js")).toBeNull();
+
+        // Put the data in the cache
+        await processingCache.writeProcessingCache(inputProcess, outputProcess, "js");
+
+        // Retrieve the data
+        expect(processingCache.readProcessingCache(inputProcess, "js")).toEqual(outputProcess);
+    });
+});
